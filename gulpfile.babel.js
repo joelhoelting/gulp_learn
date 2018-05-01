3ebbaa3191920bc2ad9f4580ee9f20d805812ec7
Joel Hoelting <joelhoelting@protonmail.com>
2018-05-01 19:21:05 -0400

    finished gulp course

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,69 +1,191 @@
 'use_strict';
 
 import gulp from 'gulp';
+import eslint from 'gulp-eslint';
 import imagemin from 'gulp-imagemin';
+import concat from 'gulp-concat';
 import newer from 'gulp-newer';
 import preprocess from 'gulp-preprocess';
+import browsersync from 'browser-sync';
 import htmlclean from 'gulp-htmlclean';
 import size from 'gulp-size'
+import imacss from 'gulp-imacss';
+import sass from 'gulp-sass';
+import pleeease from 'gulp-pleeease';
+import uglify from 'gulp-uglify';
+import urlAdjuster from 'gulp-css-url-adjuster';
 import del from 'del';
 import pkg from './package.json';
 
-var devBuild = ((process.env.NODE_ENV || 'development').trim().toLowerCase() !== 'production');
+// file locations
+var 
+	devBuild = ((process.env.NODE_ENV || 'development').trim().toLowerCase() !== 'production'),
 
-// File Locations
+	source = 'src/',
+	dest = 'build/',
 
-const PATHS = {
-  src: 'src/',
-  build: 'build/',
-  html: {
-    in: 'src/*.html',
-    watch: ['src/*.html', 'src/template/**/*'],
-    out: 'build/',
-    context: {
-      devBuild: devBuild,
-      author: pkg.author,
-      version: pkg.version
-    }
-  },
-  images: {
-    in: 'src/images/*.*',
-    out: 'build/images/'
-  }
-};
+	html = {
+		in: source + '*.html',
+		watch: [source + '*.html', source + 'template/**/*'],
+		out: dest,
+		context: {
+			devBuild: devBuild,
+			author: pkg.author,
+			version: pkg.version
+		}
+	},
 
-// Show Build Type
-console.log(`${pkg.name} ${pkg.version}, ${devBuild ? 'development' : 'production'}`)
+	images = {
+		in: source + 'images/*.*',
+		out: dest + 'images/'
+	},
 
-gulp.task('images', () => {
-  return gulp.src(PATHS.images.in)
-    .pipe(newer(PATHS.images.out))
-    .pipe(imagemin())
-    .pipe(gulp.dest(PATHS.images.out));
+	imguri = {
+		in: source + 'images/inline/*',
+		out: source + 'scss/images/',
+		filename: '_datauri.scss',
+		namespace: 'img'
+	},
+
+	css = {
+		in: source + 'scss/main.scss',
+		watch: [source + 'scss/**/*'],
+		out: dest + 'css/',
+		sassOpts: {
+			outputStyle: 'nest',
+			imagePath: '../images/',
+			precision: 3,
+			errLogToConsole: true
+		},
+		pleeeaseOpts: {
+			autoprefixer: { 
+				browsers: ['last 2 versions', '> 2%']
+			},
+			rem: ['16px'],
+			pseudoElements: true,
+			mqpacker: true,
+			minifier: !devBuild
+		}
+	},
+
+	js = {
+		in: source + 'js/**/*',
+		out: dest + 'js/',
+		filename: 'main.js'
+	},
+
+	fonts = {
+		in: source + 'fonts/*.*',
+		out: css.out + 'fonts/'
+	},
+	
+	syncOpts = {
+		server: {
+			baseDir: dest,
+			index: 'index.html'
+		},
+		open: false,
+		notify: true
+	};
+
+// show build type
+console.log(pkg.name + ' ' + pkg.version + ', ' + (devBuild ? 'development' : 'production') + ' build');
+
+// clean the build folder
+gulp.task('clean', function() {
+	del([
+		dest + '*'
+	]);
 });
 
-gulp.task('clean', () => {
-  del([
-    PATHS.build + '*'
-  ]);
+// build HTML files
+gulp.task('html', function() {
+	var page = gulp.src(html.in).pipe(preprocess({ context: html.context }));
+	if (!devBuild) {
+		page = page
+			.pipe(size({ title: 'HTML in' }))
+			.pipe(htmlclean())
+			.pipe(size({ title: 'HTML out' }));
+	}
+	return page.pipe(gulp.dest(html.out));
 });
 
-// Build HTML Files
-gulp.task('html', () => {
-  var page = gulp.src(PATHS.html.in).pipe(preprocess({context: PATHS.html.context}));
-  if (!devBuild) {
-    page = page
-      .pipe(size({ title: 'HTML in'}))
-      .pipe(htmlclean())
-      .pipe(size({ title: 'HTML out'}))
-  }
-  return page.pipe(gulp.dest(PATHS.html.out));
+// manage images
+gulp.task('images', function() {
+	return gulp.src(images.in)
+		.pipe(newer(images.out))
+		.pipe(imagemin())
+		.pipe(gulp.dest(images.out));
 });
 
-gulp.task('default', ['html', 'images'], () => {
-  // html changes
-  gulp.watch(PATHS.html.watch, ['html'])
+// convert inline images to dataURIs in SCSS source
+gulp.task('imguri', function() {
+	return gulp.src(imguri.in)
+		.pipe(imagemin())
+		.pipe(imacss(imguri.filename, imguri.namespace))
+		.pipe(gulp.dest(imguri.out));
+});
 
-  // image changes
-  gulp.watch(PATHS.images.in, ['images']);
+// copy fonts
+gulp.task('fonts', function() {
+	return gulp.src(fonts.in)
+		.pipe(newer(fonts.out))
+		.pipe(gulp.dest(fonts.out));
+});
+
+// compile sass
+gulp.task('sass', ['imguri'], function() {
+	return gulp.src(css.in)
+		.pipe(sass(css.sassOpts))
+		.pipe(urlAdjuster({
+			prepend: css.sassOpts.imagePath
+		}))
+		.pipe(size({title: 'CSS in '}))
+		.pipe(pleeease(css.pleeeaseOpts))
+		.pipe(size({title: 'CSS out '}))
+		.pipe(gulp.dest(css.out))
+		.pipe(browsersync.reload({ stream: true }));
+});
+
+// compile js
+gulp.task('js', function() {
+	if (devBuild) {
+		return gulp.src(js.in)
+			.pipe(newer(js.out))
+			.pipe(gulp.dest(js.out));
+	} else {
+		del([
+			dest + 'js/*'
+		]);
+		return gulp.src(js.in)
+			.pipe(concat(js.filename))
+			.pipe(size({ title: 'JS in '}))
+			.pipe(uglify())
+			.pipe(size({ title: 'JS out '}))
+			.pipe(gulp.dest(js.out));
+	}
+});
+
+// BrowserSync
+gulp.task('browsersync', function() {
+	browsersync(syncOpts);
+});
+
+// default task
+gulp.task('default', ['html', 'images', 'fonts', 'sass', 'js', 'browsersync'], function() {
+
+	// html changes
+	gulp.watch(html.watch, ['html', browsersync.reload]);
+
+	// image changes
+	gulp.watch(images.in, ['images']);
+
+	// font changes
+	gulp.watch(fonts.in, ['fonts']);
+
+	// sass changes
+	gulp.watch([css.watch, imguri.in], ['sass']);
+
+	// js changes
+	gulp.watch(js.in, ['js', browsersync.reload]);
 });
