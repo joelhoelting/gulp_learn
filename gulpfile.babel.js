c36e858036c6a2eb8b581a6f5881ccbd6f213a58
Joel Hoelting <joelhoelting@protonmail.com>
2018-04-30 22:32:02 -0400

    Second Commit

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,9 @@
 import gulp from 'gulp';
 import imagemin from 'gulp-imagemin';
 import newer from 'gulp-newer';
+import preprocess from 'gulp-preprocess';
+import htmlclean from 'gulp-htmlclean';
+import size from 'gulp-size'
 import del from 'del';
 import pkg from './package.json';
 
@@ -13,6 +16,16 @@ var devBuild = ((process.env.NODE_ENV || 'development').trim().toLowerCase() !==
 const PATHS = {
   src: 'src/',
   build: 'build/',
+  html: {
+    in: 'src/*.html',
+    watch: ['src/*.html', 'src/template/**/*'],
+    out: 'build/',
+    context: {
+      devBuild: devBuild,
+      author: pkg.author,
+      version: pkg.version
+    }
+  },
   images: {
     in: 'src/images/*.*',
     out: 'build/images/'
@@ -20,7 +33,7 @@ const PATHS = {
 };
 
 // Show Build Type
-// console.log(pkg.name + ' ' +)
+console.log(`${pkg.name} ${pkg.version}, ${devBuild ? 'development' : 'production'}`)
 
 gulp.task('images', () => {
   return gulp.src(PATHS.images.in)
@@ -35,6 +48,22 @@ gulp.task('clean', () => {
   ]);
 });
 
-gulp.task('default', ['images'], () => {
+// Build HTML Files
+gulp.task('html', () => {
+  var page = gulp.src(PATHS.html.in).pipe(preprocess({context: PATHS.html.context}));
+  if (!devBuild) {
+    page = page
+      .pipe(size({ title: 'HTML in'}))
+      .pipe(htmlclean())
+      .pipe(size({ title: 'HTML out'}))
+  }
+  return page.pipe(gulp.dest(PATHS.html.out));
+});
+
+gulp.task('default', ['html', 'images'], () => {
+  // html changes
+  gulp.watch(PATHS.html.watch, ['html'])
+
+  // image changes
   gulp.watch(PATHS.images.in, ['images']);
 });
